6f0292cc95abbf0665dfa4c6688e896be74298fc
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-07-24 23:44:13 -0300

    test(components): adicionado testes ao componente de upload

--- libs/code/components/src/lib/upload/upload-dropzone.directive.spec.ts
+++ libs/code/components/src/lib/upload/upload-dropzone.directive.spec.ts
@@ -6,19 +6,21 @@ describe('UploadDropzoneDirective ', () => {
   let spectator: SpectatorDirective<UploadDropzoneDirective>;
   const createDirective = createDirectiveFactory(UploadDropzoneDirective);
 
-  it('should change the background color', () => {
-    spectator = createDirective(`<div highlight>Testing UploadDropzoneDirective</div>`);
+  beforeEach(() => spectator = createDirective(`<div cndUploadDropzone>Testing UploadDropzoneDirective</div>`));
 
-    spectator.dispatchMouseEvent(spectator.element, 'mouseover');
-
-    expect(spectator.element).toHaveStyle({
-      backgroundColor: 'rgba(0,0,0, 0.1)'
-    });
-
-    spectator.dispatchMouseEvent(spectator.element, 'mouseout');
-    expect(spectator.element).toHaveStyle({
-      backgroundColor: '#fff'
-    });
+  it('should call the onDrop method when dropping files', async () => {
+    spyOn(spectator.directive, 'onDrop');
+    spectator.dispatchMouseEvent(spectator.element, 'drop');
+    expect(spectator.directive.onDrop).toHaveBeenCalled();
   });
-
+  it('should call the onDragOver method when hovering over the mouse', async () => {
+    spyOn(spectator.directive, 'onDragOver');
+    spectator.dispatchMouseEvent(spectator.element, 'dragover');
+    expect(spectator.directive.onDragOver).toHaveBeenCalled();
+  });
+  it('must call the onDragLeave method when removing the mouse from above', async () => {
+    spyOn(spectator.directive, 'onDragLeave');
+    spectator.dispatchMouseEvent(spectator.element, 'dragleave');
+    expect(spectator.directive.onDragLeave).toHaveBeenCalled();
+  });
 });
